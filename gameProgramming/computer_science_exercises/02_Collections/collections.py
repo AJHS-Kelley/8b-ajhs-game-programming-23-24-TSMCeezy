7c0fc996705efeb985666db6ab90949690c5fe25
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-09-28 14:36:39 -0400

    list contents

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-#Collections examples, Ceon gordon V0.0
+#Collections examples, Ceon gordon V0.1B
 
 #LIST -- ordered, changeable, it allows duplicates
 breakfastFoods = ["Bacon", "Waffles"] 
@@ -8,3 +8,8 @@ breakfastFoods = ["Bacon", "Waffles"]
 #Python Only: Index -1 it is the last item on the list
 testScores = [95,100,25,15,27,35]
 classGpa = [3.14,2.25,1.74,1.99,8.99,4.25]
+
+# Printing contents of an list
+print (breakfastFoods)
+print (testScores)
+print(classGpa)
