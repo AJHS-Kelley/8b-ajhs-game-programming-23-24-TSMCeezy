414c1cd9398401a02dfd6808dce8769a40fb4843
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-09-28 14:32:57 -0400

    Update collections.py

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,10 @@
 #Collections examples, Ceon gordon V0.0
+
+#LIST -- ordered, changeable, it allows duplicates
+breakfastFoods = ["Bacon", "Waffles"] 
+#Each item is a element
+#THe position in the list for each is the INDEX
+#The element "Bacon" is at idex 0
+#Python Only: Index -1 it is the last item on the list
+testScores = [95,100,25,15,27,35]
+classGpa = [3.14,2.25,1.74,1.99,8.99,4.25]
