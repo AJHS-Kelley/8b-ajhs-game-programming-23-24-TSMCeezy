7f9e9f60381f6c7b820e8fe250b2d1505d7be4fe
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-10-04 14:18:16 -0400

    Update collections.py

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,7 +1,7 @@
-#Collections examples, Ceon gordon V0.1D
+#Collections examples, Ceon gordon V0.2A
 
 #LIST -- ordered, changeable, it allows duplicates
-breakfastFoods = ["Bacon", "Waffles", "Bread"] 
+breakfastFoods = ["Bacon", "Waffles", "Bread", "h", "5", "6"] 
 #Each item is a element
 #THe position in the list for each is the INDEX
 #The element "Bacon" is at idex 0
@@ -20,11 +20,33 @@ classGpa = [3.14,2.25,1.74,1.99,8.99,4.25]
 #print(classGpa[0])
 
 #Accsesing last item in list -- use -1
-print(breakfastFoods[-1])
-print(testScores[-1])
-print(classGpa[-1])
+#print(breakfastFoods[-1])
+#print(testScores[-1])
+#print(classGpa[-1])
 #Number three on each list
-print(breakfastFoods[2])
-print(testScores[2])
-print(classGpa[2])
+#print(breakfastFoods[2])
+#print(testScores[2])
+#print(classGpa[2])
 #Changeing items in a list
+
+breakfastFoods [0] = "Sausage"
+testScores[0]= 97
+classGpa[0] = 3.57
+
+#print(breakfastFoods[0])
+#print(testScores[0])
+#print(classGpa[0])
+#print (breakfastFoods)
+#print (testScores)
+#print(classGpa)
+
+breakfastFoods[4]="fort"
+testScores[4]=7
+classGpa[4]=3.0
+
+print(breakfastFoods[4])
+print(testScores[4])
+print(classGpa[4])
+print (breakfastFoods)
+print (testScores)
+print(classGpa)
