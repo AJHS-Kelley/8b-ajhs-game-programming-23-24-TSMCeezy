1ff4883a2ede69213c7da935ffe5e8e11f6094f8
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-10-31 14:17:33 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -2,3 +2,44 @@
 
 words= 'cat dog goat frog cheeta bird fish turtle cow moose one two three four five six seven eight nine ten killer danny phanny telephone jacksonville ceon computer maximum minimum death'.split() #30 words
 print(words)
+# VARIABAL_NAMES in all caps are constants meant not to CHAnge!
+HANGMAN_BOARD= ['''
+   +---+
+       |
+       |
+       |
+    ======''','''
+   +---+
+   o   |
+       |
+       |
+    ======''','''
+   +---+
+   o   |
+   |   |
+       |
+    =======''','''
+  +---+
+   o  |
+   |\ |
+      |
+    ========''','''
+   +---+
+   o   |
+  /|\  |
+       |
+    ========''','''
+   +---+
+   o   |
+  /|\  |
+    \  |
+    ========''','''
+   +---+
+   o   |
+  /|\  |
+  / \  |
+    ========''']
+i=0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i+= 1
