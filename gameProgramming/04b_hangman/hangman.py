b99089ee736d79592a2d18d365d97846c6e0bf9f
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-10-31 14:34:16 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,5 +1,5 @@
 # Hang man game Ceon v0.1
-
+import random
 words= 'cat dog goat frog cheeta bird fish turtle cow moose one two three four five six seven eight nine ten killer danny phanny telephone jacksonville ceon computer maximum minimum death'.split() #30 words
 print(words)
 # VARIABAL_NAMES in all caps are constants meant not to CHAnge!
@@ -43,3 +43,14 @@ i=0
 while i < len(HANGMAN_BOARD):
     print(HANGMAN_BOARD[i])
     i+= 1
+
+def getRandomword(wordList): # return a random word from the list
+    wordIndex = random.randint(0,len(wordList)-1)
+    #len(listName)-1 is common for working with list.
+    return wordList[wordIndex]
+
+i = 0
+while i < 50:
+    word=getRandomword(words)
+    print(word)
+    i += 1
