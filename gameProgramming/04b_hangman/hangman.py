4910004e51ce5d6c6a78c0989fe27d9b362e9f34
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-10-31 13:58:01 -0400

    Create hangman.py

---
+++ gameProgramming/04b_hangman/hangman.py
@@ -0,0 +1,4 @@
+# Hang man game Ceon v0.1
+
+words= 'cat dog goat frog cheeta bird fish turtle cow moose one two three four five six seven eight nine ten killer danny phanny telephone jacksonville ceon computer maximum minimum death'.split() #30 words
+print(words)
