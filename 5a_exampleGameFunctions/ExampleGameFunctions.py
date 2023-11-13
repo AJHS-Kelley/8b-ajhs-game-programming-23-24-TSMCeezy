f52135e07a75c5eac4b98c1cfe0567db5844e80e
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2023-11-13 13:51:06 -0500

    making four functions and params

--- 5a_exampleGameFunctions/ExampleGameFunctions.py
+++ 5a_exampleGameFunctions/ExampleGameFunctions.py
@@ -1,1 +1,14 @@
-#Example Game Functions by Ceon Gordon
+#Example Game Functions by Ceon Gordon v0.1
+import random 
+
+def functionOne ():
+    pass
+
+def functionTwo (param1):
+    pass
+
+def functionThree(param1= "Defualt Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
