da8d5eedd6a5f7d1256ec525b4569db2efe0ca87
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-01-11 14:23:44 -0500

    Update dnaGame.py
    making

--- gameProgramming/cohort_lessons/DNA_Game/DNA GAME/dnaGame.py
+++ gameProgramming/cohort_lessons/DNA_Game/DNA GAME/dnaGame.py
@@ -3,3 +3,20 @@
 import time, datetime
 #import specific tools
 from random import choice
+dnaBases = ["A","T","G","C"]
+
+#Game Fuctions
+def gameIntro() -> None: 
+    pass
+def genDNA() -> str:
+    bassGenerated = 0
+    bassRequested = int(input("Please inter positive integer number of bases to generate.\n"))
+    dnaSequence = ""
+
+    while bassGenerated < bassRequested:
+        dnaSequence += choice(dnaBases)
+        bassGenerated += 1
+    return dnaSequence
+
+dna = genDNA()
+print (dna)
