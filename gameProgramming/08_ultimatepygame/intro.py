03f9444b3137e161e165813713b5c7a75a8d2fd8
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-01-30 14:47:42 -0500

    Update intro.py

--- gameProgramming/08_ultimatepygame/intro.py
+++ gameProgramming/08_ultimatepygame/intro.py
@@ -2,3 +2,9 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT
+    #draw all elements
+    # update everything
+    pygame.display.update()
