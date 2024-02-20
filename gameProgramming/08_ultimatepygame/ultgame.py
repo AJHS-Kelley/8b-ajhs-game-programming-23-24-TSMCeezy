909fe8870718ad91f92b6b27772fc93ddee92a34
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-02-20 14:53:35 -0500

    had too restart :{

--- gameProgramming/08_ultimatepygame/ultgame.py
+++ gameProgramming/08_ultimatepygame/ultgame.py
@@ -1,13 +1,18 @@
-#Ultpygame project by Ceon v idk
-import pygame
+import pygame #Ultpygame project by Ceon v idk
 from sys import exit
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('img/ultpy/Sky.png')
 ground_surface = pygame.image.load('img/ultpy/ground.png')
+text_surface = test_font.render('My game', False,'Black')
+
+snail_surface = pygame.image.load('img/ultpy/snail1.png')
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -15,11 +20,12 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(ground_surface(0,0))
-    screen.blit(sky_surface,(200,100))
-
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
     
-
-
     pygame.display.update()
     clock.tick(60)
