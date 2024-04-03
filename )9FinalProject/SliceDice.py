5add5f5ef199eb7ce0c5769e4081ba9e4cd8a0c4
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-04-03 13:55:00 -0400

    Update SliceDice.py

--- )9FinalProject/SliceDice.py
+++ )9FinalProject/SliceDice.py
@@ -3,3 +3,16 @@ myString = "Type a random string with punctuation, please. \n"
 
 print(alphabet[0])
 print(alphabet[-1])
+
+#splicing
+print(alphabet[0:4]) # Slice up to second index
+# Alphabet [start:stop]
+
+#slice from start
+print(alphabet[:2])
+
+#slice to the end
+print(alphabet[0:])
+
+#Slice the whole thing
+print(alphabet[:])
