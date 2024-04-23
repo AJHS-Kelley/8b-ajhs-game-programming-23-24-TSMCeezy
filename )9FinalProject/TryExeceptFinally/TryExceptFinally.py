57b8ce46323f94f87d6f4771b59b0cfd95cb0cc9
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-04-23 13:58:12 -0400

    Update TryExceptFinally.py

--- )9FinalProject/TryExeceptFinally/TryExceptFinally.py
+++ )9FinalProject/TryExeceptFinally/TryExceptFinally.py
@@ -3,6 +3,8 @@
 try:
  myVariable=1
  print(myVariable)
+ myString = "five"
+ print(float(myString))
 except NameError:#Code will run if there is a error
  print("Somthing has gone wrong in your code")
 except:
@@ -11,4 +13,3 @@ else:
  print("Code ran succefully no exceptions.\n")
 finally:
  print("Ill be back.\n")
- 
