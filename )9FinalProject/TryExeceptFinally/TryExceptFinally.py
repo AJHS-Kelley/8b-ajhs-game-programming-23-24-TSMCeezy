f71018ed0bf00c9184a665374f68d0b065c38f85
TSMCeezy <142345546+TSMCeezy@users.noreply.github.com>
2024-04-23 13:46:57 -0400

    Create TryExceptFinally.py

---
+++ )9FinalProject/TryExeceptFinally/TryExceptFinally.py
@@ -0,0 +1,14 @@
+#Try -- except -- else -- finally
+
+try:
+ myVariable=1
+ print(myVariable)
+except NameError:#Code will run if there is a error
+ print("Somthing has gone wrong in your code")
+except:
+ print(" Somthing else went wrong")
+else:
+ print("Code ran succefully no exceptions.\n")
+finally:
+ print("Ill be back.\n")
+ 
